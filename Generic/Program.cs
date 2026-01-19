57861316319fbb1de7ae2f8b85223f9fa5c4d7b0
abhay9876 <singhabhayrna987@gmail.com>
2026-01-19 11:12:50 +0530

    [Abhay Singh] Add. Generic problems solved

---
+++ Generic/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Generic.WarehouseSystem;
+using Generic.Market_Place;
+using Generic.UniversityManagement;
+using Generic.MealPlan;
+using Generic.ResumeScreeningSystem;
+namespace Generic
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+
+            Warehouse wr = new Warehouse();
+            wr.WarehouseDetail();
+
+
+
+            MarketPlace mp = new MarketPlace();
+            mp.MarketPlaceDetails();
+
+
+            University uni = new University();
+            uni.UniversityDetails();
+
+
+            Meal ml = new Meal();
+            ml.MealDetails();
+
+            ResumeScreen rs = new ResumeScreen();
+            rs.Screening();
+        }
+    }
+}
